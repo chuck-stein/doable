121b408bb0f560281dfda160bcc4e51877f64df4
Chuck Stein <cfgstein@gmail.com>
2025-05-13 20:29:54 -0400

    Update version name to 0.1.0 and version code to 2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "io.chuckstein.doable"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0"
     }
     packaging {
         resources {
